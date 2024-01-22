1809369be3a1b327317bcb4703f18237bfe85228
teerapol43 <Teerapol.ph@rmuti.ac.th>
2024-01-22 13:17:10 +0700

    push

--- client/src/layout/HeaderBar.js
+++ client/src/layout/HeaderBar.js
@@ -34,12 +34,7 @@ const HeaderBar = () => {
         <Box display="flex" justifyContent="space-between" p={2}>
             {/* search  */}
             <Box display="flex" borderRadius="3px" backgroundColor="#F5EFE7">
-                <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
-                <IconButton type="button" sx={{ p: 1 }}>
-                    <SearchIcon />
-                </IconButton>
             </Box>
-
             {/* icons */}
             <Box display="flex">
                 <IconButton>
